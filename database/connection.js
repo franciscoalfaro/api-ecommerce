563c18b454b1770b69007b7f458a3ce80906e299
franciscoalfar@gmail.com <*Fran2811*>
2024-09-27 19:56:16 -0300

    config db

--- database/connection.js
+++ database/connection.js
@@ -1,30 +1,19 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const connection = async () => {
+const connection = async()=>{
     try {
-        let mongoURI;
-
-        // Verifica si estamos en un entorno de producción
-        if (process.env.NODE_ENV === 'production') {
-            // Si es producción, utiliza la variable de entorno para la URI de MongoDB de producción
-            mongoURI = process.env.PROD_MONGODB_URI;
-            console.log('Conectando a la base de datos de producción:');
-        } else {
-            // Si no es producción, utiliza la variable de entorno para la URI de MongoDB de desarrollo
-            mongoURI = process.env.DEV_MONGODB_URI;
-            console.log('Conectando a la base de datos de desarrollo:');
-        }
-
-        await mongoose.connect(mongoURI);
-        console.log("Conexión exitosa a la base de datos");
-
+        await mongoose.connect(process.env.MONGODB_URI,)
+        console.log("Connection success BD Gastos")
+        
     } catch (error) {
-        console.error("Error de conexión a la base de datos:", error);
+        console.log(error);
         console.log('Intentando nuevamente la conexión en 2 segundos...');
         setTimeout(connection, 2000);
+        
     }
-};
+}
+
 
 module.exports = {
     connection 
